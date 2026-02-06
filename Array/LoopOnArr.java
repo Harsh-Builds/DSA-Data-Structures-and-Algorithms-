1592e0463ee29545a918246e306ac5137b3e51b2
Harsh-Builds <harshsaini888555@gmail.com>
2026-02-06 22:40:06 +0530

    try looping on string type of array

---
+++ Array/LoopOnArr.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class LoopOnArr{
+
+    public static void main(String[] args){
+
+        String[] fruits = {"mango", "apple", "banana", "watermelon"};
+
+        int len = fruits.length;
+   
+        // System.out.println(len);
+
+        for(int i=0; i < len; i++){
+
+            System.out.println(fruits[i]);
+        }
+    }
+}
