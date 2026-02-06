998fa3720845b5bdfc55e776c2b48ff6a7d81359
Harsh-Builds <harshsaini888555@gmail.com>
2026-02-06 22:30:15 +0530

    use basics of array

---
+++ Array/Printarr.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Printarr {
+    
+    public static void main(String[] args){
+
+        int[] myarr = new int[5];
+
+        myarr[0] = 23;
+        myarr[1] = 34;
+        myarr[2] = 65;
+        myarr[3] = 60;
+        myarr[4] = 43;
+
+        System.out.println(myarr[3]);
+    }
+}
